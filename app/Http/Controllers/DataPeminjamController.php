5b1e708eadad6e421da3679852d2ac2b0823d967
Marcellinus <ignacymischa@gmail.com>
2022-06-22 22:25:23 +0700

    first commit

---
+++ app/Http/Controllers/DataPeminjamController.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DataPeminjam;
+use Illuminate\Http\Request;
+use App\Models\Telepon;
+use App\Models\JenisKelamin;
+use Session;
+use Storage;
+
+class DataPeminjamController extends Controller
+{
+    public function index()
+    {
+        $jumlah_peminjam = DataPeminjam::count();
+        $data_peminjam = DataPeminjam::orderBy('id', 'asc')->paginate(5);
+        $no = 0;
+        return view('data_peminjam.index', compact('data_peminjam', 'no',  'jumlah_peminjam'));
+    }
+
+    public function search(Request $request)
+    {
+        $batas = 5;
+        $cari = $request->kata;
+        $data_peminjam = DataPeminjam::where('nama_peminjam', 'like', '%'.$cari.'%')->paginate($batas);
+        $no = $batas * ($data_peminjam->currentPage() - 1);
+        return view('data_peminjam.search', compact('data_peminjam', 'no', 'cari'));
+    }
+
+    public function create()
+    {
+        // return view('data_peminjam.create');
+        $list_jenis_kelamin = JenisKelamin::pluck('nama_jenis_kelamin' , 'id_jenis_kelamin');
+        return view('data_peminjam.create', compact('list_jenis_kelamin'));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'kode_peminjam' => 'required|string',
+            'nama_peminjam' => 'required|string|max:30',
+            'tanggal_lahir' => 'required|date'
+        ]);
+
+        $this->validate($request, [
+            'foto' => 'required|image|mimes:jpeg,jpg,png',
+        ]);
+        $foto_peminjam = $request->foto;
+        $nama_file = time().'.'.$foto_peminjam->getClientOriginalExtension();
+        $foto_peminjam->move('foto_peminjam/', $nama_file);
+
+        $data_peminjam = new DataPeminjam;
+        $data_peminjam->kode_peminjam = $request->kode_peminjam;
+        $data_peminjam->nama_peminjam = $request->nama_peminjam;
+        $data_peminjam->id_jenis_kelamin = $request->id_jenis_kelamin;
+        $data_peminjam->tanggal_lahir = $request->tanggal_lahir;
+        $data_peminjam->alamat = $request->alamat;
+        $data_peminjam->pekerjaan = $request->pekerjaan;
+        $data_peminjam->foto = $nama_file;
+        $data_peminjam->save();
+
+        $telepon = new Telepon;
+        $telepon->nomor_telepon = $request->telepon;
+        $data_peminjam->telepon()->save($telepon);
+
+        Session::flash('flash_message', 'Data peminjam berhasil disimpan');
+        return redirect('data_peminjam');
+    }
+
+    public function edit($id)
+    {
+        $peminjam = DataPeminjam::find($id);
+        if (!empty($peminjam->telepon->nomor_telepon)) {
+            $peminjam->nomor_telepon = $peminjam->telepon->nomor_telepon;
+        }
+        $list_jenis_kelamin = JenisKelamin::pluck('nama_jenis_kelamin', 'id_jenis_kelamin');
+        return view('data_peminjam.edit', compact('peminjam', 'list_jenis_kelamin'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data_peminjam = DataPeminjam::find($id);
+        if($request->has('foto')){
+            $foto_peminjam = $request->foto;
+            $nama_file = time().'.'.$foto_peminjam->getClientOriginalExtension();
+            $foto_peminjam->move('foto_peminjam/', $nama_file);
+            $data_peminjam->kode_peminjam = $request->kode_peminjam;
+            $data_peminjam->nama_peminjam = $request->nama_peminjam;
+            $data_peminjam->id_jenis_kelamin = $request->id_jenis_kelamin;
+            $data_peminjam->tanggal_lahir = $request->tanggal_lahir;
+            $data_peminjam->alamat = $request->alamat;
+            $data_peminjam->pekerjaan = $request->pekerjaan;
+            $data_peminjam->foto = $nama_file;
+            $data_peminjam->update();
+        } else{
+            $data_peminjam->kode_peminjam = $request->kode_peminjam;
+            $data_peminjam->nama_peminjam = $request->nama_peminjam;
+            $data_peminjam->id_jenis_kelamin = $request->id_jenis_kelamin;
+            $data_peminjam->tanggal_lahir = $request->tanggal_lahir;
+            $data_peminjam->alamat = $request->alamat;
+            $data_peminjam->pekerjaan = $request->pekerjaan;
+            $data_peminjam->update();
+        }
+        // update nomor telepon, jika sebelumnya suda ada nomor telepon
+        if ($data_peminjam->telepon) {
+            // jika telepon diisi, maka update
+            if ($request->filled('nomor_telepon')) {
+                $telepon = $data_peminjam->telepon;
+                $telepon->nomor_telepon = $request->input('nomor_telepon');
+                $data_peminjam->telepon()->save($telepon);
+            } else {
+                $data_peminjam->telepon()->delete();
+            }
+        }
+        // buat entry baru, jika sebelumnya tidak ada nomor telepon
+        else {
+            if ($request->filled('nomor_telepon')) {
+                $telepon = new Telepon;
+                $telepon->nomr_telepon = $request->nomor_telepon;
+                $data_peminjam->telepon()->save($telepon);
+            }
+        }
+
+        Session::flash('flash_message', 'Data peminjam berhasil diupdate');
+        return redirect('data_peminjam');
+    }
+
+    public function destroy($id)
+    {
+        $data_peminjam = DataPeminjam::find($id);
+        $data_peminjam->delete();
+
+        Session::flash('flash_message', 'Data Peminjam berhasil dihapus');
+        Session::flash('penting', true);
+        return redirect('data_peminjam');
+    }
+
+    public function cobaCollection()
+    {
+        $daftar = [
+            'Budi Pranoto',
+            'Amy Delia',
+            'Cakra Lukman',
+            'Dewi Nova',
+            'Kartini Indah'
+        ];
+        $collection = collect($daftar)->map(function ($nama) {
+            return ucwords($nama);
+        });
+        return $collection;
+    }
+
+    public function collection_first()
+    {
+        $collection = DataPeminjam::all()->first();
+        return $collection;
+    }
+
+    public function collection_last()
+    {
+        $collection = DataPeminjam::all()->last();
+        return $collection;
+    }
+
+    public function collection_count()
+    {
+        $collection = DataPeminjam::all();
+        $jumlah = $collection->count();
+        return 'jumlah peminjam : ' . $jumlah;
+    }
+
+    public function collection_take()
+    {
+        $collection = DataPeminjam::all()->take(3);
+        return $collection;
+    }
+
+    public function collection_pluck()
+    {
+        $collection = DataPeminjam::all()->pluck('nama_peminjam');
+        return $collection;
+    }
+
+    public function collection_where()
+    {
+        $collection = DataPeminjam::all()->where('kode_peminjam', 'P0004');
+        return $collection;
+    }
+
+    public function collection_wherein()
+    {
+        $collection = DataPeminjam::all()->whereIn('kode_peminjam', ['P0004', 'P0002']);
+        return $collection;
+    }
+
+    public function collection_toarray()
+    {
+        $collection = DataPeminjam::select('kode_peminjam', 'nama_peminjam')->take(3)->get();
+        $koleksi = $collection->toArray();
+        foreach ($koleksi as $peminjam) {
+            echo $peminjam['kode_peminjam'] . ' - ' . $peminjam['nama_peminjam'] . '<br>';
+        }
+    }
+
+    public function collection_tojson()
+    {
+        $data = [
+            ['kode_peminjam' => 'P0001', 'nama_peminjam' => 'Karina'],
+            ['kode_peminjam' => 'P0002', 'nama_peminjam' => 'Joko'],
+            ['kode_peminjam' => 'P0003', 'nama_peminjam' => 'Zainal'],
+            ['kode_peminjam' => 'P0004', 'nama_peminjam' => 'Basuki Wiryawan'],
+        ];
+        $collection = collect($data)->toJson();
+        return $collection;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,141 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\IndexController@index');
+
+Route::get('home', function(){
+    return view('home');
+});
+
+Route::get('peminjaman', 'App\Http\Controllers\PeminjamanController@index');
+
+Route::get('peminjaman/create', 'App\Http\Controllers\PeminjamanController@create')->name('peminjaman.create');
+
+Route::POST('peminjaman/store', 'App\Http\Controllers\PeminjamanController@store')->name('peminjaman.store');
+
+Route::get('peminjaman/detail_peminjam/{id}', 'App\Http\Controllers\PeminjamanController@detail_peminjam')->name('peminjaman.detail_peminjam');
+
+Route::get('peminjaman/detail_buku/{id}', 'App\Http\Controllers\PeminjamanController@detail_buku')->name('peminjaman.detail_buku');
+
+Route::get('data_peminjam', 'App\Http\Controllers\DataPeminjamController@index');
+
+Route::get('data_peminjam/create', 'App\Http\Controllers\DataPeminjamController@create')->name('data_peminjam.create');
+
+Route::post('data_peminjam/store', 'App\Http\Controllers\DataPeminjamController@store')->name('data_peminjam.store');
+
+Route::get('data_peminjam/edit/{id}', 'App\Http\Controllers\DataPeminjamController@edit')->name('data_peminjam.edit');
+
+Route::post('data_peminjam/update/{id}', 'App\Http\Controllers\DataPeminjamController@update')->name('data_peminjam.update');
+
+Route::post('data_peminjam/delete/{id}', 'App\Http\Controllers\DataPeminjamController@destroy')->name('data_peminjam.destroy');
+
+Route::get('data_peminjam/search', 'App\http\Controllers\DataPeminjamController@search')->name('data_peminjam.search');
+
+Route::get('coba_collection', 'App\Http\Controllers\DataPeminjamController@cobacollection');
+
+Route::get('collection_first', 'App\Http\Controllers\DataPeminjamController@collection_first');
+
+Route::get('collection_last', 'App\Http\Controllers\DataPeminjamController@collection_last');
+
+Route::get('collection_count', 'App\Http\Controllers\DataPeminjamController@collection_count');
+
+Route::get('collection_take', 'App\Http\Controllers\DataPeminjamController@collection_take');
+
+Route::get('collection_pluck', 'App\Http\Controllers\DataPeminjamController@collection_pluck');
+
+Route::get('collection_where', 'App\Http\Controllers\DataPeminjamController@collection_where');
+
+Route::get('collection_wherein', 'App\Http\Controllers\DataPeminjamController@collection_wherein');
+
+Route::get('collection_toarray', 'App\Http\Controllers\DataPeminjamController@collection_toarray');
+
+Route::get('collection_tojson', 'App\Http\Controllers\DataPeminjamController@collection_tojson');
+
+Route::get('lihat_data_peminjam', 'App\Http\Controllers\PeminjamController@lihat_data_peminjam');
+
+
+
+Route::get('/biodata', function(){
+    return 'Nama : Kiko <br> NIM : 123456789 <br> Alamat : Semarang <br> Hobi : rebahan';
+});
+
+Route::get('mahasiswa/{prodi}', function($prodi){
+    return 'Mahasiswa Program Studi : '.$prodi;
+});
+
+Route::get('mahasiswa2/{prodi?}', function($prodi=null){
+    if($prodi == null)
+        return "Data Program Studi kosong";
+    return "Mahasiswa Program Studi : ".$prodi;
+});
+
+Route::get('mahasiswa3/{prodi?}', function($prodi="Teknologi Rekayasa Komputer"){
+    return "Mahasiswa prodi : ".$prodi;
+});
+
+Route::get('biodata2', function(){
+    return view('biodata2');
+});
+
+Route::group([], function(){
+    Route::get('/pertama', function()
+    {
+        echo "Route pertama";
+    });
+    Route::get('/kedua', function()
+    {
+        echo "Route kedua";
+    });
+    Route::get('/ketiga', function()
+    {
+        echo "Route ketiga";
+    });
+});
+
+Route::group(['prefix' => 'latihan'], function(){
+    Route::get('/satu', function()
+    {
+        echo "Latihan 1";
+    });
+    Route::get('/dua', function()
+    {
+        echo "Latihan 2";
+    });
+    Route::get('/tiga', function()
+    {
+        echo "Latihan 3";
+    });
+});
+
+Route::group(array('prefix' => 'admin'), function(){
+    Route::get('/', function()
+    {
+        return 'Halaman Home Admin';
+    });
+    Route::get('posts', function()
+    {
+        return "Halaman input data buku";
+    });
+    Route::get('posts/simpan', function()
+    {
+        return "Data berhasil disimpan";
+    });
+});
+
+Route::name('kuliah')->group(function(){
+    Route::get('Teknologi Rekayasa Komputer', function(){
+        return "Kuliah di Program Studi Teknologi Rekayasa Komputer";
+    });
+});
